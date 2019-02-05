d140b70a36fe218ac4d045386d2b0a1f549fda65
James Hippler <james.hippler@logicmonitor.com>
2019-02-05 12:42:13 -0600

    Created program to demonstrate basic datatypes in Groovy

--- simple_data_types.groovy
+++ simple_data_types.groovy
@@ -42,18 +42,24 @@ println("Min Integer Size: $Integer.MIN_VALUE")
 // long:
 // Min Value: -9,223,372,036,854,775,808 (2^63)
 // Max Value: 9,223,372,036,845,775,807 (2^63 -1)
-long l = -9223372036854775808
-println("long: $l")
-println(l.getClass())
+long l1 = -9223372036854775808
+long l2 = 100L
+println("long 1: $l1")
+println("long 2: $l2")
+println(l1.getClass())
+println(l2.getClass())
 println("Max Long Size: $Long.MAX_VALUE")
 println("Min Long Size: $Long.MIN_VALUE")
 
 // float: 32-bit IEEE floating points (single precision)
 // Min Value: 1.4E-45
 // Max Value: 3.4028235E38
-float f = 1.4E-45
-println("float: $f")
-println(f.getClass())
+float f1 = 1.4E-45
+float f2 = 1.25F
+println("float 1: $f1")
+println("float 2: $f2")
+println(f1.getClass())
+println(f2.getClass())
 println("Max Float Size: $Float.MAX_VALUE")
 println("Min Float Size: $Float.MIN_VALUE")
 
@@ -61,8 +67,26 @@ println("Min Float Size: $Float.MIN_VALUE")
 // Min Value: 4.9E-324
 // Max Value: 1.7976931348623157E308
 
-double d = 1.7976931348623157E308
-println("double: $d")
-println(d.getClass())
+double d1 = 1.7976931348623157E308
+double d2 = 1.25
+println("double 1: $d1")
+println("double 2: $d2")
+println(d1.getClass())
+println(d2.getClass())
 println("Max Double Size: $Double.MAX_VALUE")
 println("Min Double Size: $Double.MIN_VALUE")
+
+// char: single character or unicode
+char c = 'c'
+println("char: $c")
+println(c.getClass())
+println("Max Char Size: Any character or Unicode")
+
+// boolean: holds True or False
+boolean bool1 = true
+boolean bool2 = false
+println("bool 1: $bool1")
+println("bool 2: $bool2")
+println(bool1.getClass())
+println(bool2.getClass())
+println("Max Boolean Size: True or False Values only")
