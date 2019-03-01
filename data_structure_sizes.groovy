34fc46c49e49f77ab39e793637d18398ef6f25f8
James Hippler <james.hippler@logicmonitor.com>
2019-03-01 09:15:53 -0600

    Added file that simply shows the min/max values of each primitive data type

---
+++ data_structure_sizes.groovy
@@ -0,0 +1,8 @@
+#! /usr/bin/env groovy
+println("BYTE\nSize:\t$Byte.SIZE\nMin:\t$Byte.MIN_VALUE\nMax:\t$Byte.MAX_VALUE\n")
+println("SHORT\nSize:\t$Short.SIZE\nMin:\t$Short.MIN_VALUE\nMax:\t$Short.MAX_VALUE\n")
+println("CHARACTER\nSize:\t$Character.SIZE\nMin:\t${(int)Character.MIN_VALUE}\nMax:\t${(int)Character.MAX_VALUE}\n")
+println("INTEGER\nSize:\t$Integer.SIZE\nMin:\t$Integer.MIN_VALUE\nMax:\t$Integer.MAX_VALUE\n")
+println("LONG\nSize:\t$Long.SIZE\nMin:\t$Long.MIN_VALUE\nMax:\t$Long.MAX_VALUE\n")
+println("FLOAT\nSize:\t$Float.SIZE\nMin:\t$Float.MIN_VALUE\nMax:\t$Float.MAX_VALUE\n")
+println("DOUBLE\nSize:\t$Double.SIZE\nMin:\t$Double.MIN_VALUE\nMax:\t$Double.MAX_VALUE\n")
